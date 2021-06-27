241f780c58f75701c4cfa2e16698466cc595bf6c
higashida <cervus--ht7578@ezweb.ne.jp>
2021-06-27 12:07:15 +0000

    質問

--- resources/js/fullcalendar.js
+++ resources/js/fullcalendar.js
@@ -16,12 +16,20 @@ document.addEventListener('DOMContentLoaded', function () {
     initialView: 'timeGridWeek',
     locales: [ jaLocale],
     locale: 'ja',
-    
     slotMinTime: '08:00:00',
     slotMaxTime: '18:00:00',
     contentHeigh: 'auto',
     height: 'auto',
-    events: []
+    googleCalendarApiKey: 'AIzaSyDq0vOKEt3DRYR1Fz9-_hEDKENAwuh7cdI',
+     eventSources: [
+        {
+          googleCalendarId: 'ja.japanese#holiday@group.v.calendar.google.com',
+          className: 'event_holiday'
+        },
+        {
+          googleCalendarId: 'hoge-na-calendar@group.calendar.google.com',
+        }
+      ]
   });
   
   calendar.render();
